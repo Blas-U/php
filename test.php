fbe9f7fb86f9dd63cc7667a2e754065b5f2b80c6
Blas-U <cheezypizzagamer@gmail.com>
2023-02-09 13:56:32 -0600

    update

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+echo"Hi <b>Brooke</b>, you are late ";
+echo"and you are too coen!!!<br> ";
+
+
+print_r($_GET) . "<br>";
+echo "Hello " . $_GET["name"] . "<p>";
+
+foreach ($_GET as $id => $val){
+    echo $id . "==>" . $val . "<br>";
+}
+
+echo '<input type="button" value="Say Hi!">';
+ 
+
+$f=fopen("test.json", "a");#open file
+fwrite($f, "this is a file.\n");#write something
+fwrite()
+fclose($f);#close file
+echo "Saved";
+
+?>
